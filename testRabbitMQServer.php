77ebc19f6518e1686511b8beba6ceb807fd1e733
Peter Kolesnik <pete885@gmail.com>
2018-09-23 21:49:00 -0400

    Database setup

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -32,9 +32,7 @@ function requestProcessor($request)
 
 $server = new rabbitMQServer("testRabbitMQ.ini","testServer");
 
-echo "testRabbitMQServer BEGIN".PHP_EOL;
 $server->process_requests('requestProcessor');
-echo "testRabbitMQServer END".PHP_EOL;
 exit();
 ?>
 
