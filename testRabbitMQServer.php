3a759e5acf502a62c95eedf793d71af20c502505
marc-e3 <me236@njit.edu>
2018-09-25 12:54:24 -0400

    Attempt at trying to retrieve username and see if it is in the database

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -1,5 +1,54 @@
 #!/usr/bin/php
 <?php
+
+//Setting the loginDB login information for mysql
+$servername = "localhost";
+$username = "dbAdmin";
+$password = "password123!";
+$dbname = "loginDB";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+//Checking if connection is established and relaying the status in a message
+if(!$conn)
+{
+        die("Connection failed: " . mysqli_connect_error());
+}
+if($conn)
+{
+        die("Connection Successful!");
+}
+
+//Setting the username received from the front end to the variable login
+$login=$_POST['username'];
+
+//Checking to see if the username is empty
+if($username!='')
+{
+	$query = "select * from username where login='".$username."'" or die("Connection failed: " . mysqli_connect_error());
+	$res = mysqli_fetch_row($query);
+	if($res)
+	{
+		$_SESSION['username']=$login;
+		echo "welcome to php";
+	}
+	else
+	{
+		echo 'You entered a username not found in the database';
+	}
+}
+else
+{
+	echo 'Enter username';
+}
+
+function ActuallyDoLogin($username, $password)
+{
+
+	return true;
+}
+
+
+
+
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
