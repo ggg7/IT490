84ff5b56841b8aa70c365ec4fab9586e0720c9e0
Polskipete <pete885@gmail.com>
2018-10-01 14:24:00 -0400

    re did backend

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -1,6 +1,29 @@
 #!/usr/bin/php
 <?php
-require_once('path.inc');
+	$user = "peter";
+	$pass = "1234";
+	$servername = "localhost";
+	$username = "dbAdmin";
+	$password = "password123!";
+	$dbname = "loginDB";
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+	if(!$conn)
+	{
+		printf("Connection Failed: ".mysqli_connect_error());
+	}
+	if($conn)
+	{
+		printf("Connection Succesful!");
+	}
+	
+	$result = mysqli_query($conn, "select * from loginTable where username = '$user' and password ='$pass'") or die("Failed to query".mysqli_error());
+	$row = mysqli_fetch_array($result);
+	echo($row['username']);
+	echo($row['password']);
+	
+
+require_once('path.inc');	
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
